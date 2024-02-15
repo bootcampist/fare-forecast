4bf872bab22a89d191ea804f53c83e7e1a951e01
Phoenix <bootcampgithub@gmail.com>
2024-02-15 06:29:06 +0000

    re-establish access to currency exchange

--- assets/js/Sidcurrencycode.js
+++ assets/js/Sidcurrencycode.js
@@ -1,5 +1,7 @@
 // Sid's currency exchange code
-const currencyAPI = 'https://v6.exchangerate-api.com/v6/cce8439a497e07a97ad9f20f/pair/';
+// const currencyAPI = 'https://v6.exchangerate-api.com/v6/cce8439a497e07a97ad9f20f/pair/';
+const  a = 'eb3d55a074f43b30d0a257a0';
+const currencyAPI = `https://v6.exchangerate-api.com/v6/${a}/pair/`;
 const locationAPI = "https://api.openweathermap.org/geo/1.0/direct?q=";
 const locationAPIkey = "&appid=7029ec148d2666f47569499650a8ea2e";
 const databaseLink = "https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/616b1fb83cbfd4eb6d9e7d52924bb00a/country-codes_json.json";
